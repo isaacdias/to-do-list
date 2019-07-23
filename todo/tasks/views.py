77f3bfc2d39338c925b87345fc0c4c3b92542d05
Isaac Dias <isaacardias@gmail.com>
2019-07-23 20:03:34 -0300

    criação view tasklist git status

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def tasklist(request):
+    task = Task.objects.all().order_by('create_at')
+    return render(request, 'task/list.html', {'task':tasks})
