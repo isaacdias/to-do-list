abe1b82a0408cf834a2c733dc9ca95f989b7ca86
Isaac Dias <isaacardias@gmail.com>
2019-07-24 10:14:18 -0300

    correção sintaxe da view tasklist

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def tasklist(request):
-    task = Task.objects.all().order_by('create_at')
-    return render(request, 'task/list.html', {'tasks':tasks})
+    #task = Task.objects.all().order_by('create_at')
+    return render(request, 'tasks/list.html')
